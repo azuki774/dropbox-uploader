a960848b027430aa09af56b08a2dbb7ab355719a
azuki774 <azuki774s@gmail.com>
2022-09-01 23:41:37 +0900

    delete unnecessary construction

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -4,14 +4,6 @@ import (
 	"go.uber.org/zap"
 )
 
-type UploadOptions struct {
-	Logger      *zap.Logger
-	SrcDir      string // File or Directory
-	DstDir      string // Dropbox directory
-	OverWrite   bool
-	AccessToken string
-}
-
 func NewLogger() (Logger *zap.Logger, err error) {
 	config := zap.NewProductionConfig()
 	// config.Level = zap.NewAtomicLevelAt(zap.DebugLevel)
