eec85fb453763bb84421aad953e3b4eef983a2dd
azuki774 <azuki774s@gmail.com>
2023-02-19 23:12:52 +0900

    delete tokenrepo info

--- cmd/dropbox-uploader/start.go
+++ cmd/dropbox-uploader/start.go
@@ -1,22 +1,8 @@
 package cmd
 
 import (
-	"azuki774/dropbox-uploader/internal/factory"
-	"azuki774/dropbox-uploader/internal/logger"
-	"fmt"
-
-	"github.com/robfig/cron/v3"
 	"github.com/spf13/cobra"
 )
-
-type StartOption struct {
-	RepoInfo struct {
-		Host string
-		Port string
-	}
-}
-
-var startOpt StartOption
 
 // startCmd represents the upload command
 var startCmd = &cobra.Command{
@@ -31,44 +17,10 @@ to quickly create a Cobra application.`,
 	SilenceUsage:  true,
 	SilenceErrors: true,
 	RunE: func(cmd *cobra.Command, args []string) error {
-		client := factory.NewClient()
-		l, err := logger.NewLogger()
-		if err != nil {
-			fmt.Println(err)
-			return err
-		}
-
-		tr := factory.NewTokenRepo(startOpt.RepoInfo.Host, startOpt.RepoInfo.Port)
-		newTokenClient, err := factory.NewNewTokenClient(tr)
-		if err != nil {
-			fmt.Println(err)
-			return err
-		}
-
-		tkRepo := factory.NewTokenRepo(startOpt.RepoInfo.Host, startOpt.RepoInfo.Port)
-		us := factory.NewUsecases(l, client, newTokenClient, tkRepo)
-		srv := factory.NewServer(l, us)
-
-		err = us.GetNewAccessToken()
-		if err != nil {
-			return err
-		}
-
-		c := cron.New()
-		c.AddFunc("@hourly", func() { us.GetNewAccessToken() })
-		c.Start()
-
-		err = srv.Start()
-		if err != nil {
-			return err
-		}
-
 		return nil
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(startCmd)
-	startCmd.Flags().StringVar(&startOpt.RepoInfo.Host, "repo-host", "localhost", "token repository host")
-	startCmd.Flags().StringVar(&startOpt.RepoInfo.Port, "repo-port", "80", "token repositroy port")
 }
--- internal/factory/factory.go
+++ internal/factory/factory.go
@@ -1,53 +1,24 @@
 package factory
 
 import (
-	"azuki774/dropbox-uploader/internal/client"
-	"azuki774/dropbox-uploader/internal/server"
-	"azuki774/dropbox-uploader/internal/usecases"
-	"fmt"
-	"net/http"
-	"os"
+	"time"
 
 	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
-func NewUsecases(logger *zap.Logger, client client.Client, nclient client.NewTokenClient, tkRepo client.TokenRepo) *usecases.Usecases {
-	return &usecases.Usecases{Logger: logger, Client: &client, NewTokenClient: &nclient, TokenRepo: &tkRepo}
+func NewLogger() (Logger *zap.Logger, err error) {
+	config := zap.NewProductionConfig()
+	config.EncoderConfig.EncodeTime = JSTTimeEncoder
+	// config.Level = zap.NewAtomicLevelAt(zap.DebugLevel)
+	l, err := config.Build()
+
+	l.WithOptions(zap.AddStacktrace(zap.ErrorLevel))
+	return l, err
 }
 
-func NewClient() client.Client {
-	return client.Client{}
-}
-
-func NewNewTokenClient(tr client.TokenRepo) (c client.NewTokenClient, err error) {
-	// Refresh Token
-	oa, err := tr.Get()
-	if err != nil {
-		fmt.Printf("failed to get refresh_token from repository: %s\n", err.Error())
-		return client.NewTokenClient{}, err
-	}
-	refToken := oa.RefreshToken
-
-	// App Key
-	appKey, ok := os.LookupEnv("APP_KEY")
-	if !ok {
-		return c, fmt.Errorf("failed to load APP_KEY")
-	}
-
-	// App Secret
-	appSecret, ok := os.LookupEnv("APP_SECRET")
-	if !ok {
-		return c, fmt.Errorf("failed to load APP_SECRET")
-	}
-
-	c = client.NewTokenClient{Client: &http.Client{}, RefreshToken: refToken, AppKey: appKey, AppSecret: appSecret}
-	return c, nil
-}
-
-func NewServer(l *zap.Logger, us *usecases.Usecases) server.Server {
-	return server.Server{Host: "", Port: "80", Logger: l, Usecase: us}
-}
-
-func NewTokenRepo(host string, port string) client.TokenRepo {
-	return client.TokenRepo{Host: host, Port: port}
+func JSTTimeEncoder(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
+	const layout = "2006-01-02T15:04:05+09:00"
+	jst := time.FixedZone("Asia/Tokyo", 9*60*60)
+	enc.AppendString(t.In(jst).Format(layout))
 }
